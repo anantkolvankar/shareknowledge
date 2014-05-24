335ccfdf297ae27b3ad1267f651acdbe1eca87e5
Adnan <adnankh07@gmail.com>
2014-05-24 14:08:31 +0530

    Added seed data for populating categories

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -28,7 +28,7 @@ class NotesController < ApplicationController
 
     respond_to do |format|
       if @note.save
-        format.html { redirect_to @note, notice: 'Note was successfully created.' }
+        format.html { redirect_to dashboard_url(:id => @note.project_id), notice: 'Note was successfully created.' }
         format.json { render action: 'show', status: :created, location: @note }
       else
         format.html { render action: 'new' }
